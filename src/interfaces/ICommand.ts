db6c015158fa3c8150aa0415414ad0934feb022b
Daniel.Manea <danimane4info@gmail.com>
2023-03-18 23:59:52 +0000

    New Interface for commands

--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -1,12 +1,6 @@
-import { ChatInputApplicationCommandData, Client, CommandInteraction } from "discord.js";
+import {CommandInteraction, SlashCommandBuilder } from "discord.js";
 
-export interface ICommand extends ChatInputApplicationCommandData {
-    run: ({
-        client,
-        interaction,
-    } : {
-        client?: Client,
-        interaction: CommandInteraction,
-    }
-    ) => void;
+export interface ICommand extends SlashCommandBuilder {
+    data: SlashCommandBuilder
+    run: (interaction: CommandInteraction) => Promise<void>
 }
