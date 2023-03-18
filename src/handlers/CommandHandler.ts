f4251a3d5ea327b3b71f4f85899a8d567a76ab30
Daniel.Manea <danimane4info@gmail.com>
2023-03-18 23:59:27 +0000

    New command handler. (might change this after)

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -1,20 +1,20 @@
-import { Client, CommandInteraction, Interaction } from "discord.js";
-import { Commands } from "../utils/Commands";
+import { Client } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
 
-export default (client: Client): void => {
-  client.on("interactionCreate", async (interaction: Interaction) => {
-      if (!interaction.isCommand() && !interaction.isContextMenuCommand()) return;
-      await handleSlashCommand(client, interaction);
-  });
-};
+export const CommandHandler = (client: Client) => {
+  const parentDirName = path.join(__dirname, '../');
+  const commandPaths = path.resolve(parentDirName, 'commands');
+  const commandFolders = fs.readdirSync(commandPaths);
 
-const handleSlashCommand = async (client: Client, interaction: CommandInteraction): Promise<void> => {
-  const slashCommand = Commands.find(c => c.name === interaction.commandName);
-  if (!slashCommand) {
-      return;
+  for (const commandFolder of commandFolders) {
+    const commandPath = path.join(parentDirName, 'commands', commandFolder);
+    const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.ts'));
+
+    for (const file of commandFiles) {
+      const filePath = path.join(commandPath, file);
+      const command = require(filePath).default;
+      client.once(command.name, (...args: any[]) => command.run(...args));
+    }
   }
-
-  await interaction.deferReply();
-
-  slashCommand.run({client, interaction});
 };
